2c5df2451fbda64be095b31b270c922d537995f2
Olwyn Patterson <odonogol@tcd.ie>
2022-04-02 17:39:54 +0100

    Stop Times class

--- FinalProject/Stop_Times.java
+++ FinalProject/Stop_Times.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //Class for reading in stop_times.txt file
 public class Stop_Times {
 	
@@ -10,10 +15,38 @@ public class Stop_Times {
 	public int pickup_type;
 	public int drop_off_type;
 	public double shape_dist_traveled;
+	public ArrayList<Stop_Times> stop_times;
 	
-	Stop_Times(){
-		
+	Stop_Times(int trip_id, double arrival_time, double departure_time){
+		this.trip_id = trip_id;
+		this.arrival_time = arrival_time;
+		this.departure_time = departure_time;	
 	}
+	
+	private void Stop_Times() {
+        try {
+            String filename = "Input Files\stop_times.txt";
+            File file = new File(filename);
+            Scanner sc = new Scanner(file);
+            sc.useDelimiter(",");
+            sc.nextLine();
+            while (sc.hasNext()) {
+                if (sc.hasNextInt()) {
+                    int tripID = sc.nextInt();
+                    sc.next();
+                    double arrivalTime = sc.nextDouble();
+                    sc.next();
+                    double departureTime = sc.nextDouble();
+                    sc.next();
+                    stop_times.add(new Stop_Times(tripID, arrivalTime, departureTime));
+                    sc.nextLine();
+                }
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            stop_times = null;
+        }
+    }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
