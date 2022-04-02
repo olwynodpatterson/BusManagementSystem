fbe9a5ffe644dd0c68b34a39431e54be6ac14eb6
Olwyn Patterson <odonogol@tcd.ie>
2022-04-02 17:29:32 +0100

    Stops class work

--- FinalProject/Stops.java
+++ FinalProject/Stops.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //Class for reading in stops.txt file
 public class Stops {
 	public int stop_id;
@@ -10,13 +15,39 @@ public class Stops {
 	public String stop_url;
 	public String location_type;
 	public String parent_station;
-	
-	Stops(){
-		
+
+	public ArrayList<Stops> stops;
+
+	Stops(int stop_id, String stop_name){
+		this.stop_id = stop_id;
+		this.stop_name = stop_name;
 	}
+	
+	 private void Stops() {
+	        try {
+	            String filename = "Input Files\stops.txt";
+	            File file = new File(filename);
+	            Scanner sc = new Scanner(file);
+	            sc.useDelimiter(",");
+	            sc.nextLine();
+	            while (sc.hasNext()) {
+	                if (sc.hasNextInt()) {
+	                    int stopID = sc.nextInt();
+	                    sc.next();
+	                    String stopName = sc.next();
+	                    stops.add(new Stops(stopID, stopName));
+	                    sc.nextLine();
+	                }
+	            }
+	            sc.close();
+	        } catch (FileNotFoundException e) {
+	            stops = null;
+	        }
+	    }
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 
 	}
 
