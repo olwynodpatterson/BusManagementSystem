41a85b9d57a3ec37aee4077253cea7bf79034938
Olwyn Patterson <odonogol@tcd.ie>
2022-04-06 18:00:03 +0100

    6/4/22 work

--- FinalProject/TST.java
+++ FinalProject/TST.java
@@ -2,86 +2,260 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-//Adapted from book
+/******************************************************************************
+ *  Compilation:  javac TST.java
+ *  Execution:    java TST < words.txt
+ *  Dependencies: StdIn.java
+ *  Data files:   https://algs4.cs.princeton.edu/52trie/shellsST.txt
+ *
+ *  Symbol table with string keys, implemented using a ternary search
+ *  trie (TST).
+ *
+ *
+ *  % java TST < shellsST.txt
+ *  keys(""):
+ *  by 4
+ *  sea 6
+ *  sells 1
+ *  she 0
+ *  shells 3
+ *  shore 7
+ *  the 5
+ *
+ *  longestPrefixOf("shellsort"):
+ *  shells
+ *
+ *  keysWithPrefix("shor"):
+ *  shore
+ *
+ *  keysThatMatch(".he.l."):
+ *  shells
+ *
+ *  % java TST
+ *  theory the now is the time for all good men
+ *
+ *  Remarks
+ *  --------
+ *    - can't use a key that is the empty string ""
+ *
+ ******************************************************************************/
 
-public class TST<Value> 
-{
-	public void fileToTST(String filename) {
-		try {
-			File file = new File(filename);
-			Scanner sc = new Scanner(file);
-			sc.useDelimiter(",");
-			sc.nextLine();
-			while (sc.hasNext()) {
-				if (sc.hasNextInt()) {
-					int stopID = sc.nextInt();
-					sc.next();
-					String stopName = sc.next();
-					if (stopName.startsWith("WB") || stopName.startsWith("NB") || stopName.startsWith("SB") || stopName.startsWith("EB")) {
-						//do something to split string and add prefix to end
-						String [] stopNameSplit = stopName.split(" ");
-						String stopNameUpdate = "";
-						for (int i = 1; i < stopNameSplit.length; i ++) {
-							String stopNameTemp = stopNameSplit[i];
-							stopNameUpdate.concat(stopNameTemp + " ");
-						}
-						String stopNameFinal = stopNameUpdate.concat(stopNameSplit[0]);	
-						Stops stops = new Stops(stopID, stopNameFinal);
-						put(stops.stop_name, stops);
-						sc.nextLine();
-					}
-					else {
-						Stops stops = new Stops(stopID, stopName);
-						put(stops.stop_name, stops);
-						sc.nextLine();
-					}
+//package edu.princeton.cs.algs4;
 
-				}
-			}
-			sc.close();
-		} catch (FileNotFoundException e) {
-			//stops = null;
-		}
-	}
+/**
+ *  The {@code TST} class represents an symbol table of key-value
+ *  pairs, with string keys and generic values.
+ *  It supports the usual <em>put</em>, <em>get</em>, <em>contains</em>,
+ *  <em>delete</em>, <em>size</em>, and <em>is-empty</em> methods.
+ *  It also provides character-based methods for finding the string
+ *  in the symbol table that is the <em>longest prefix</em> of a given prefix,
+ *  finding all strings in the symbol table that <em>start with</em> a given prefix,
+ *  and finding all strings in the symbol table that <em>match</em> a given pattern.
+ *  A symbol table implements the <em>associative array</em> abstraction:
+ *  when associating a value with a key that is already in the symbol table,
+ *  the convention is to replace the old value with the new value.
+ *  Unlike {@link java.util.Map}, this class uses the convention that
+ *  values cannot be {@code null}—setting the
+ *  value associated with a key to {@code null} is equivalent to deleting the key
+ *  from the symbol table.
+ *  <p>
+ *  This implementation uses a ternary search trie.
+ *  <p>
+ *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/52trie">Section 5.2</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ */
+public class TST<Stops> {
+    private int n;              // size
+    private Node<Stops> root;   // root of TST
+
+    private static class Node<Stops> {
+        private char c;                        // character
+        private Node<Stops> left, mid, right;  // left, middle, and right subtries
+        private Stops val;                     // value associated with string
+    }
+
+    /**
+     * Initializes an empty string symbol table.
+     */
+    public TST() {
+    }
+
+    /**
+     * Returns the number of key-value pairs in this symbol table.
+     * @return the number of key-value pairs in this symbol table
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Does this symbol table contain the given key?
+     * @param key the key
+     * @return {@code true} if this symbol table contains {@code key} and
+     *     {@code false} otherwise
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public boolean contains(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+        return get(key) != null;
+    }
+
+    /**
+     * Returns the value associated with the given key.
+     * @param key the key
+     * @return the value associated with the given key if the key is in the symbol table
+     *     and {@code null} if the key is not in the symbol table
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public Stops get(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls get() with null argument");
+        }
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        Node<Stops> x = get(root, key, 0);
+        if (x == null) return null;
+        return x.val;
+    }
+
+    // return subtrie corresponding to given key
+    private Node<Stops> get(Node<Stops> x, String key, int d) {
+        if (x == null) return null;
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        char c = key.charAt(d);
+        if      (c < x.c)              return get(x.left,  key, d);
+        else if (c > x.c)              return get(x.right, key, d);
+        else if (d < key.length() - 1) return get(x.mid,   key, d+1);
+        else                           return x;
+    }
+
+    /**
+     * Inserts the key-value pair into the symbol table, overwriting the old value
+     * with the new value if the key is already in the symbol table.
+     * If the value is {@code null}, this effectively deletes the key from the symbol table.
+     * @param key the key
+     * @param stops the value
+     * @throws IllegalArgumentException if {@code key} is {@code null}
+     */
+    public void put(String key, Stops stops) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with null key");
+        }
+        if (!contains(key)) n++;
+        else if(stops == null) n--;       // delete existing key
+        root = put(root, key, stops, 0);
+    }
+
+    private Node<Stops> put(Node<Stops> x, String key, Stops val, int d) {
+        char c = key.charAt(d);
+        if (x == null) {
+            x = new Node<Stops>();
+            x.c = c;
+        }
+        if      (c < x.c)               x.left  = put(x.left,  key, val, d);
+        else if (c > x.c)               x.right = put(x.right, key, val, d);
+        else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
+        else                            x.val   = val;
+        return x;
+    }
+
+    /**
+     * Returns the string in the symbol table that is the longest prefix of {@code query},
+     * or {@code null}, if no such string.
+     * @param query the query string
+     * @return the string in the symbol table that is the longest prefix of {@code query},
+     *     or {@code null} if no such string
+     * @throws IllegalArgumentException if {@code query} is {@code null}
+     */
+    public String longestPrefixOf(String query) {
+        if (query == null) {
+            throw new IllegalArgumentException("calls longestPrefixOf() with null argument");
+        }
+        if (query.length() == 0) return null;
+        int length = 0;
+        Node<Stops> x = root;
+        int i = 0;
+        while (x != null && i < query.length()) {
+            char c = query.charAt(i);
+            if      (c < x.c) x = x.left;
+            else if (c > x.c) x = x.right;
+            else {
+                i++;
+                if (x.val != null) length = i;
+                x = x.mid;
+            }
+        }
+        return query.substring(0, length);
+    }
+
+    /**
+     * Returns all keys in the symbol table as an {@code Iterable}.
+     * To iterate over all of the keys in the symbol table named {@code st},
+     * use the foreach notation: {@code for (Key key : st.keys())}.
+     * @return all keys in the symbol table as an {@code Iterable}
+     */
+    public Iterable<String> keys() {
+        Queue<String> queue = new Queue<String>();
+        collect(root, new StringBuilder(), queue);
+        return queue;
+    }
+
+    /**
+     * Returns all of the keys in the set that start with {@code prefix}.
+     * @param prefix the prefix
+     * @return all of the keys in the set that start with {@code prefix},
+     *     as an iterable
+     * @throws IllegalArgumentException if {@code prefix} is {@code null}
+     */
+    public Iterable<String> keysWithPrefix(String prefix) {
+        if (prefix == null) {
+            throw new IllegalArgumentException("calls keysWithPrefix() with null argument");
+        }
+        Queue<String> queue = new Queue<String>();
+        Node<Stops> x = get(root, prefix, 0);
+        if (x == null) return queue;
+        if (x.val != null) queue.enqueue(prefix);
+        collect(x.mid, new StringBuilder(prefix), queue);
+        return queue;
+    }
+
+    // all keys in subtrie rooted at x with given prefix
+    private void collect(Node<Stops> x, StringBuilder prefix, Queue<String> queue) {
+        if (x == null) return;
+        collect(x.left,  prefix, queue);
+        if (x.val != null) queue.enqueue(prefix.toString() + x.c);
+        collect(x.mid,   prefix.append(x.c), queue);
+        prefix.deleteCharAt(prefix.length() - 1);
+        collect(x.right, prefix, queue);
+    }
 
 
-	private Node root; // root of trie
-	private class Node
-	{
-		char c; // character
-		Node left, mid, right; // left, middle, and right subtries
-		Stops val; // value associated with string
-	}
-	public Stops get(String key) 
-	{
-		Node x = get(root, key, 0);
-		if (x == null) return null;
-		return (Stops) x.val;
-	}
-	private Node get(Node x, String key, int d)
-	{
-		if (x == null) return null;
-		char c = key.charAt(d);
-		if (c < x.c) return get(x.left, key, d);
-		else if (c > x.c) return get(x.right, key, d);
-		else if (d < key.length() - 1)
-			return get(x.mid, key, d+1);
-		else return x;
-	}
-
-	public void put(String key, Stops val){
-		root = put(root, key, val, 0); 
-	}
-
-	private Node put(Node x, String key, Stops val, int d)
-	{
-		char c = key.charAt(d);
-		if (x == null) { x = new Node(); x.c = c; }
-		if (c < x.c) x.left = put(x.left, key, val, d);
-		else if (c > x.c) x.right = put(x.right, key, val, d);
-		else if (d < key.length() - 1)
-			x.mid = put(x.mid, key, val, d+1);
-		else x.val = val;
-		return x;
-	}
+    /**
+     * Returns all of the keys in the symbol table that match {@code pattern},
+     * where the character '.' is interpreted as a wildcard character.
+     * @param pattern the pattern
+     * @return all of the keys in the symbol table that match {@code pattern},
+     *     as an iterable, where . is treated as a wildcard character.
+     */
+    public Iterable<String> keysThatMatch(String pattern) {
+        Queue<String> queue = new Queue<String>();
+        collect(root, new StringBuilder(), 0, pattern, queue);
+        return queue;
+    }
+ 
+    private void collect(Node<Stops> x, StringBuilder prefix, int i, String pattern, Queue<String> queue) {
+        if (x == null) return;
+        char c = pattern.charAt(i);
+        if (c == '.' || c < x.c) collect(x.left, prefix, i, pattern, queue);
+        if (c == '.' || c == x.c) {
+            if (i == pattern.length() - 1 && x.val != null) queue.enqueue(prefix.toString() + x.c);
+            if (i < pattern.length() - 1) {
+                collect(x.mid, prefix.append(x.c), i+1, pattern, queue);
+                prefix.deleteCharAt(prefix.length() - 1);
+            }
+        }
+        if (c == '.' || c > x.c) collect(x.right, prefix, i, pattern, queue);
+    }
 }
+
--- FinalProject/UserInterface.java
+++ FinalProject/UserInterface.java
@@ -1,7 +1,94 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //User Interface of the project
 public class UserInterface {
+	
+	public static boolean validTime(String time) {
+		String timeFormat = "((\\s?)[0-9]|[01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
+		Pattern pattern = Pattern.compile(timeFormat);
+		if (time == null) {
+			return false;
+		}
+		Matcher matcher = pattern.matcher(time);
+		return matcher.matches();
+		
+	}
+	
+	private static ArrayList readInStopTimes(String filename) {
+		ArrayList<Stop_Times> stopTimes = new ArrayList();
+		try {
+			File file = new File(filename);
+			Scanner sc = new Scanner(file);
+			sc.useDelimiter(",");
+			sc.nextLine();
+			while (sc.hasNext()) {
+				if (sc.hasNextInt()) { 
+					int tripID = sc.nextInt();
+					String arrivalTime = sc.next();
+					String departureTime = sc.next();
+					if(validTime(arrivalTime)) {
+						stopTimes.add(new Stop_Times(tripID, arrivalTime, departureTime));
+						sc.nextLine();
+					}
+					else sc.nextLine();					
+				}
+			}
+			sc.close();
+			return stopTimes;
+		} catch (FileNotFoundException e) {
+			stopTimes = null;
+			return null;
+		}
+		
+	}
+	
+	public static String editName(String name) {
+		String finalName = "";
+		if(name.startsWith("FLAGSTOP") || name.startsWith("WB") || name.startsWith("NB") || name.startsWith("SB") || name.startsWith("EB")){
+			String [] stopNameSplit = name.split(" ");
+			for (int i = 1; i < stopNameSplit.length; i ++) {
+				String stopNameTemp = stopNameSplit[i];
+				finalName = finalName.concat(stopNameTemp + " ");
+			}
+			finalName = finalName.concat(stopNameSplit[0]);
+			return finalName;
+		}
+		else return name;
+	}
+	
+	 public static TST fileToTST(String filename) {
+		 TST TST = new TST();
+			try {
+				File file = new File(filename);
+				Scanner sc = new Scanner(file);
+				sc.useDelimiter(",");
+				sc.nextLine();
+				while (sc.hasNext()) {
+					if (sc.hasNextInt()) {
+						int stopID = sc.nextInt();
+						sc.next();
+						String stopName = sc.next();
+						String stopNameUpdate1 = editName(stopName);
+						String stopNameUpdate2 = editName(stopNameUpdate1);
+						Stops stops = new Stops(stopID, stopNameUpdate2);
+						TST.put(stops.stop_name, stops);
+						sc.nextLine();
+
+					}
+				}
+				sc.close();
+				return TST;
+			} catch (FileNotFoundException e) {
+				return null;
+			}
+		}
+	    
+	
 
 	public static void main(String[] args) {
 
@@ -24,26 +111,50 @@ public class UserInterface {
 			}
 			else if(userInput.equals("B")) {
 				//Search bus stops
-
 				System.out.print("Enter bus stop full name or first few characters :");
 				String userInputStop = input.nextLine();
-				//symbol table will have [stops.getname, stops] 
-				//then will search for user input in keys
-				TST TST = new TST();
 				String file = "Input Files\\stops.txt";
-				TST.fileToTST(file);
-				if(TST.get(userInputStop) != null){  //need to be able to handle only a few characters of stop name
-					TST.get(userInputStop);
-					System.out.print("Success. Bus stop in system.");
-				}
-				else {
-					System.out.print("Error. Bus stop not in system.");
+				TST<Stops> busStops = fileToTST(file);
+				Iterable<String> stops = busStops.keysWithPrefix(userInputStop);
+				int counter = 0;
+				for (String stop : stops) {
+					counter++;
+					if(counter == 1) {
+						System.out.println("Bus stop found:");
+					}
+					else if(counter == 0) {
+						System.out.print("Sorry. There were no stops found.");
+					}
+					Stops stopOutput =  busStops.get(stop);	
+					System.out.println("\nStop Name: " + stopOutput.stop_name + ", Stop ID: " + stopOutput.stop_id);
+					
 				}
 
 
 			}
 			else if (userInput.equals("C")) {
 				// Search with arrival times
+				System.out.print("Enter an arrival time as hh:mm:ss : ");
+				String userInputTime = input.nextLine();
+				String filename = "Input Files\\stop_times.txt";
+				ArrayList<Stops> arr = new ArrayList();
+				validTime(userInputTime);
+				arr = readInStopTimes(filename);
+				if(validTime(userInputTime)) {
+					System.out.print("Valid time");
+					for(int i=0; i<arr.size(); i++) {
+						Stops tempStop = new Stops();
+						tempStop =  arr.get(i);
+					}
+
+					
+//					else {
+//						System.out.print("Sorry. There were no stops found for that time.");
+//					}
+				}
+				else {
+					System.out.println("Error. Invalid time");
+				}
 			}
 			else{
 				System.out.println("Error. Invalid input");
